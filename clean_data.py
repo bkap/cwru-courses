30c27aed899c7ca9c8fc05bab40668856275dd91
Stephen Johnson <srj15@case.edu>
2009-12-02 23:45:36 -0500

    All subject codes now categorized

---
+++ clean_data.py
@@ -0,0 +1,14 @@
+from data import *
+
+orphans = set()
+for course in all_courses:
+    cn = course['coursename'][:4]
+    if     cn not in SCHOOL_OF_ENGINEERING \
+       and cn not in SAGES \
+       and cn not in ARTS_AND_SCIENCES \
+       and cn not in WEATHERHEAD \
+       and cn not in MEDICAL \
+       and cn not in MANDEL \
+       and cn not in KNOWN_ORPHAN:
+        orphans.add(cn)
+print orphans
