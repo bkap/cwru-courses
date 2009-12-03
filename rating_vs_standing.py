3b77b975553a53f41f8ec1a32add45555b5db7fd
Stephen Johnson <srj15@case.edu>
2009-12-03 11:03:40 -0500

    More prettiness (mostly labels)

--- rating_vs_standing.py
+++ rating_vs_standing.py
@@ -2,6 +2,11 @@ print 'loading data...'
 
 from data import *
 
+x_values = [0.0, 1.0, 2.0, 3.0, 4.0]
+x_labels = ['\nFreshman', '\nSophomore', '\nJunior', '\nSenior', '\nGraduate']
+y_values = [0.0, 1.0, 2.0, 3.0, 4.0]
+y_labels = ['E', 'VG', 'G', 'F', 'P']
+
 print 'parsing data...'
 
 standings = []
@@ -25,8 +30,12 @@ import pylab as p
 
 print 'plotting data...'
 
+p.title('Overall Rating vs University Standing')
+p.xticks(x_values, x_labels, linespacing=2.0)
+p.yticks(y_values, y_labels)
 p.plot(standings, rankings, 'k,', ms=1, alpha=0.2)
 p.plot([0.0, 4.0], [fit[1], fit[1] + 4.0*fit[0]], 'r-')
+p.legend(['rating', 'regression'], 'lower right')
 p.savefig('paper/figures/rating_vs_standing.pdf')
 
 import subprocess
