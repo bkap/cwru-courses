b94749c22c05eb7bcaee8d34a8a97aa2c4491f27
Stephen Johnson <srj15@case.edu>
2009-12-02 23:46:05 -0500

    Thar she plots

--- rating_vs_standing.py
+++ rating_vs_standing.py
@@ -1,5 +1,28 @@
+print 'loading data...'
+
 from data import *
+
+print 'parsing data...'
+
+standings = []
+rankings = []
 
 with open('ratings_standings.csv', 'w') as f:
     for course in all_courses:
-        f.write("%s, %s\n" % (getScore(course, STANDING), getScore(course, COURSE_RANKING)))
+        standings.append(getScore(course, STANDING))
+        rankings.append(getScore(course, COURSE_RANKING))
+        # f.write("%s, %s\n" % (getScore(course, STANDING), getScore(course, COURSE_RANKING)))
+
+print "calculating correlation coefficient..."
+import numpy
+
+print numpy.corrcoef(standings, rankings)
+
+print 'importing matplotlib...'
+
+import pylab as p
+
+print 'plotting data...'
+
+p.plot(standings, rankings, 'bo', ms=1)
+p.show()
